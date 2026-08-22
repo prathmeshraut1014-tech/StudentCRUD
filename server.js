b6b1dc810d88d80cd417a1cb89b215352c6d9a84
Prathmesh Raut <prathmeshraut1014@gmail.com>
2026-08-23 00:22:32 +0530

    link

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
 
         const PORT = process.env.PORT || 5000;
 
-        app.listen(PORT, () => {
+        app.listen(PORT,"0.0.0.0", () => {
             console.log(`Server running on port ${PORT}`);
         });
     })
